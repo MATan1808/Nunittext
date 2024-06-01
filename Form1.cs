cec6597a5b70fcd330c8cc83ca50f00f861a5a79
MATan1808 <105353487+MATan1808@users.noreply.github.com>
2024-06-02 00:44:45 +0700

    Nguyễn Thị Thanh Mai, Nunit

--- Form1.cs
+++ Form1.cs
@@ -7,10 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using Keys = OpenQA.Selenium.Keys;
-namespace Selenium_46_NguyenPhuocTho
+
+namespace GiaTriLonNhat_22_NguyenThiThanhMai
 {
     public partial class Form1 : Form
     {
@@ -19,131 +17,56 @@ namespace Selenium_46_NguyenPhuocTho
             InitializeComponent();
         }
 
-        private void btn_46_NguyenPhuocTho_Click(object sender, EventArgs e)
+        private void btn_22_NguyenThiThanhMai_Click(object sender, EventArgs e)
         {
-            
-            ///////////////////////////////////////////////////////////////////////
-            //
-            ChromeDriverService chrome_46NguyenPhuocTho = ChromeDriverService.CreateDefaultService();
-            chrome_46NguyenPhuocTho.HideCommandPromptWindow = true;
-            ////dieu huong trinh duyet
-        
-            //
-            IWebDriver driver_46NguyenPhuocTho = new ChromeDriver(chrome_46NguyenPhuocTho);
-            driver_46NguyenPhuocTho.Navigate().GoToUrl("https://practice.automationtesting.in/");
-            
-            // XPath thêm vài giỏ hàng
-            driver_46NguyenPhuocTho.FindElement(By.XPath("//*[@id=\"text-22-sub_row_1-0-2-0-0\"]/div/ul/li/a[2]")).Click();
-            //// đợi load dl
-            System.Threading.Thread.Sleep(1000);
-            driver_46NguyenPhuocTho.FindElement(By.XPath("//*[@id=\"text-22-sub_row_1-0-2-1-0\"]/div/ul/li/a[2]")).Click();
+            // Kiểm tra xem dữ liệu nhập vào từ TextBox có hợp lệ không
+            if (!string.IsNullOrWhiteSpace(txta_22_NguyenThiThanhMai.Text))
+            {
+                // Tách chuỗi nhập vào thành các phần tử của mảng
+                string[] inputValues = txta_22_NguyenThiThanhMai.Text.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
-            //thêm vào giỏ hàng nhiều lần xem có lỗi
+                // Tạo mảng số nguyên với kích thước bằng độ dài của chuỗi nhập vào
+                int[] numbers = new int[inputValues.Length];
 
-            //for (int i = 0; i < 60; i++)
-            //{
-            //    driver_46NguyenPhuocTho.FindElement(By.XPath("//*[@id=\"text-22-sub_row_1-0-2-0-0\"]/div/ul/li/a[2]")).Click();
-            //    driver_46NguyenPhuocTho.FindElement(By.XPath("//*[@id=\"text-22-sub_row_1-0-2-1-0\"]/div/ul/li/a[2]")).Click();
-            //    driver_46NguyenPhuocTho.FindElement(By.XPath("//*[@id=\"text-22-sub_row_1-0-2-2-0\"]/div/ul/li/a[2]")).Click();
-            //}
-            ////
-            /////Nếu giỏ hàng không có gì thì đứa đến trang shop ngược lại thì đưa đến trang basket
-            //driver_46NguyenPhuocTho.FindElement(By.ClassName("wpmenucart-contents")).Click();
-            // nhiều lần ko lỗi
-            //for (int i = 0; i < 60; i++)
-            //{
-            //    driver_46NguyenPhuocTho.FindElement(By.ClassName("wpmenucart-contents")).Click();
-            //}
-            //Chọn Sản Phẩm ở Trang chủ->xem thông tin -> thêm vào giỏ hàng
-            //driver_46NguyenPhuocTho.FindElement(By.ClassName("woocommerce-Price-currencySymbol")).Click();
-            //driver_46NguyenPhuocTho.FindElement(By.ClassName("single_add_to_cart_button")).Click();
-            //chọn trang shop-> thêm sp
-            driver_46NguyenPhuocTho.FindElement(By.LinkText("Shop")).Click();
-            driver_46NguyenPhuocTho.FindElement(By.XPath("//*[@id=\"content\"]/ul/li[1]/a[2]")).Click();
-            driver_46NguyenPhuocTho.FindElement(By.XPath("//*[@id=\"content\"]/ul/li[2]/a[2]")).Click();
-            driver_46NguyenPhuocTho.FindElement(By.XPath("//*[@id=\"content\"]/ul/li[3]/a[2]")).Click();
-            driver_46NguyenPhuocTho.FindElement(By.XPath("//*[@id=\"content\"]/ul/li[4]/a[2]")).Click();
-            driver_46NguyenPhuocTho.FindElement(By.XPath("//*[@id=\"content\"]/ul/li[5]/a[2]")).Click();
-            driver_46NguyenPhuocTho.FindElement(By.XPath("//*[@id=\"content\"]/ul/li[6]/a[2]")).Click();
-            driver_46NguyenPhuocTho.FindElement(By.XPath("//*[@id=\"content\"]/ul/li[7]/a[2]")).Click();
-            driver_46NguyenPhuocTho.FindElement(By.XPath("//*[@id=\"content\"]/ul/li[8]/a[2]")).Click();
-            //chọn trang shop -> xem thông tin sp ->thêm sản phẩm
-           
-            driver_46NguyenPhuocTho.FindElement(By.XPath("//*[@id=\"content\"]/ul/li[1]/a[1]/img")).Click();
-            driver_46NguyenPhuocTho.FindElement(By.ClassName("single_add_to_cart_button")).Click();
+                // Nhập dữ liệu vào mảng từ chuỗi nhập vào
+                for (int i = 0; i < inputValues.Length; i++)
+                {
+                    // Kiểm tra xem giá trị nhập vào có hợp lệ không
+                    if (int.TryParse(inputValues[i], out int value))
+                    {
+                        numbers[i] = value; // Gán giá trị vào mảng
+                    }
+                    else
+                    {
+                        MessageBox.Show("Dữ liệu nhập vào không hợp lệ!"); // Thông báo lỗi nếu dữ liệu không hợp lệ
+                        return;
+                    }
+                }
+                // Gọi hàm TimPhanTuLonNhat để tìm giá trị lớn nhất trong mảng
+                int max = TimPhanTuLonNhat_22_NguyenThiThanhMai(numbers);
 
-            ////--------------------------------------------- THANH TOÁN---------------------------
-            // driver_46NguyenPhuocTho.FindElement(By.ClassName("wc-proceed-to-checkout")).Click();
-            // //nhiều lần thì lỗi
-            // //driver_46NguyenPhuocTho.FindElement(By.ClassName("wc-proceed-to-checkout")).Click();
-            // //driver_46NguyenPhuocTho.FindElement(By.ClassName("wc-proceed-to-checkout")).Click();
-            // driver_46NguyenPhuocTho.FindElement(By.Id("place_order")).Click();
-            // //không nhập thông tin
-            // //buộc nhập các thông tin sau:
-            // //Billing First Name is a required field.
-            // //Billing Last Name is a required field.
-            // //Billing Email Address is a required field.
-            // //Billing Phone is a required field.
-            // //Billing Address is a required field.
-            // //Billing Town / City is a required field.
-            // //Billing Postcode / ZIP is a required field
-            ////nhập thông tin first name
-            // IWebElement FirstNameInput_46NguyenPhuocTho = driver_46NguyenPhuocTho.FindElement(By.Name("billing_first_name"));
-            // FirstNameInput_46NguyenPhuocTho.SendKeys("46_NguyenPhuocTho");
-            ////nhập thông tin last name
-            // IWebElement LastNameInput_46NguyenPhuocTho = driver_46NguyenPhuocTho.FindElement(By.Name("billing_last_name"));
-            // LastNameInput_46NguyenPhuocTho.SendKeys("46_NguyenPhuocTho");
-            ////nhập thông tin company
-            // IWebElement CompanyInput_46NguyenPhuocTho = driver_46NguyenPhuocTho.FindElement(By.Name("billing_company"));
-            // CompanyInput_46NguyenPhuocTho.SendKeys("46_NguyenPhuocTho");
-            ////nhập thông tin email
-            // IWebElement EmailInput_46NguyenPhuocTho = driver_46NguyenPhuocTho.FindElement(By.Name("billing_email"));
-            // EmailInput_46NguyenPhuocTho.SendKeys("46_NguyenPhuocTho@gmail.com");
-            ////nhập thông tin sdt
-            // IWebElement PhoneInput_46NguyenPhuocTho = driver_46NguyenPhuocTho.FindElement(By.Name("billing_phone"));
-            // PhoneInput_46NguyenPhuocTho.SendKeys("4646464646");
-
-            // //tên quốc gia
-
-            // // Tìm và click vào phần tử input để mở danh sách kết quả
-            // IWebElement inputElementQG_46NguyenPhuocTho = driver_46NguyenPhuocTho.FindElement(By.CssSelector("span.select2-chosen"));
-            // inputElementQG_46NguyenPhuocTho.Click();
-
-            // // Tìm và click vào phần tử "Albania" trong danh sách kết quả
-            // IWebElement albaniaOption = driver_46NguyenPhuocTho.FindElement(By.CssSelector("li.select2-results-dept-0[role='presentation']:nth-child(3) div.select2-result-label"));
-            // albaniaOption.Click();
-            ////nhập thông tin địa chỉ
-            // IWebElement AddressInput_46NguyenPhuocTho = driver_46NguyenPhuocTho.FindElement(By.Name("billing_address_1"));
-            // AddressInput_46NguyenPhuocTho.SendKeys("46_Tho");
-            ////nhập thông tin dia chi
-            // IWebElement Address2Input_46NguyenPhuocTho = driver_46NguyenPhuocTho.FindElement(By.Name("billing_address_2"));
-            // Address2Input_46NguyenPhuocTho.SendKeys("46_NguyenPhuocTho");
-            ////nhập thông tin city
-            // IWebElement CityInput_46NguyenPhuocTho = driver_46NguyenPhuocTho.FindElement(By.Name("billing_city"));
-            // CityInput_46NguyenPhuocTho.SendKeys("4646464646");
-            ////nhập thông tin state
-            // IWebElement StateInput_46NguyenPhuocTho = driver_46NguyenPhuocTho.FindElement(By.Name("billing_state"));
-            // StateInput_46NguyenPhuocTho.SendKeys("4646464646");
-            ////nhập thông tin code
-            // IWebElement PostcodeInput_46NguyenPhuocTho = driver_46NguyenPhuocTho.FindElement(By.Name("billing_postcode"));
-            // PostcodeInput_46NguyenPhuocTho.SendKeys("464");
-            ////Chọn radioButton Phương thức thanh toán
-            // IWebElement CKradioButton_46_NguyenPhuocTho = driver_46NguyenPhuocTho.FindElement(By.Id("payment_method_cod"));
-            // CKradioButton_46_NguyenPhuocTho.Click();
-            // //IWebElement KTradioButton_46_NguyenPhuocTho = driver_46NguyenPhuocTho.FindElement(By.Id("payment_method_bacs"));
-            // //KTradioButton_46_NguyenPhuocTho.Click();
-            // //IWebElement GHradioButton_46_NguyenPhuocTho = driver_46NguyenPhuocTho.FindElement(By.Id("payment_method_cheque"));
-            // //GHradioButton_46_NguyenPhuocTho.Click();
-            // //IWebElement PPradioButton_46_NguyenPhuocTho = driver_46NguyenPhuocTho.FindElement(By.Id("payment_method_ppec_paypal"));
-            // //PPradioButton_46_NguyenPhuocTho.Click();
-            ////chọn Thanh toán
-            //driver_46NguyenPhuocTho.FindElement(By.Id("place_order")).Click();
-
-
-
-
-
-
+                // Hiển thị giá trị lớn nhất tìm được trên TextBox txtb
+                txtb_22_NguyenThiThanhMai.Text = max.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập một số nguyên!"); // Thông báo lỗi nếu dữ liệu nhập vào không hợp lệ
+            }
         }
+
+        private int TimPhanTuLonNhat_22_NguyenThiThanhMai(int[] mangSoNguyen_22_NguyenThiThanhMai)
+        {
+            if (mangSoNguyen_22_NguyenThiThanhMai == null || mangSoNguyen_22_NguyenThiThanhMai.Length == 0)
+                throw new ArgumentException("Mảng rỗng hoặc không tồn tại");
+
+            int max = mangSoNguyen_22_NguyenThiThanhMai[0];
+            foreach (var phanTu in mangSoNguyen_22_NguyenThiThanhMai)
+            {
+                if (phanTu > max)
+                    max = phanTu;
+            }
+            return max;
+        }
+
     }
 }
